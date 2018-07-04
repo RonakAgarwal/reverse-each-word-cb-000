57972880dfe13f11740fcbff0e3a4c0b87ffe5cc
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-04 19:10:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def burger(toppings)
-  toppings.collect do |topping|
+
+
+def reverse_each_word(string)
+  string.collect do |word|
     "I love #{topping} on my burgers"
   end
 end
-
-puts burger(pizza)
