69721810f7c04a886dfa5b35ba67725211fb94fc
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-04 19:07:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 
 def reverse_each_word(phrase)
-  result = phrase.collect do |word|
+  phrase.collect do |word|
     word.reverse
   end
-  result.join(" ")
 end
 
 reverse_each_word("Hello there, and how are you?")
